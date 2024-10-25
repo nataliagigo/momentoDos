2e500ae14db72e53582486cdf92a2ab09f575cb5
nataliagigo <nataliagigo@hotmail.es>
2024-10-25 11:36:52 -0500

    se hace validacion de datos para pedir otro ingreso en ejercicioSeis.js

--- view/public/js/ejercicioSeis.js
+++ view/public/js/ejercicioSeis.js
@@ -1,19 +1,23 @@
-let opcion
-do{
-let consumoEnergia = Number(prompt('Ingrese el consumo de la vivienda (en kWh): '))
-let costoEnergia
+let opcion;
+do {
+  let consumoEnergia = Number(
+    prompt("Ingrese el consumo de la vivienda (en kWh): ")
+  );
+  let costoEnergia;
 
-if (consumoEnergia <= 100) {
-    costoEnergia = 150 * consumoEnergia
-}else if(consumoEnergia >=101  && consumoEnergia <= 300){
-    costoEnergia = 200 * consumoEnergia
-}else{
-    costoEnergia = 250 * consumoEnergia
-}
+  if (consumoEnergia <= 100) {
+    costoEnergia = 150 * consumoEnergia;
+  } else if (consumoEnergia >= 101 && consumoEnergia <= 300) {
+    costoEnergia = 200 * consumoEnergia;
+  } else {
+    costoEnergia = 250 * consumoEnergia;
+  }
 
-console.log('El consumo de energia fue: ' + consumoEnergia);
-console.log('El costo total de energía es: ' + costoEnergia);
-console.log('------------------------');
+  console.log("El consumo de energia fue: " + consumoEnergia);
+  console.log("El costo total de energía es: " + costoEnergia);
+  console.log("------------------------");
 
-opcion = prompt('Desea ingresar otra vivienda? (1. si - 2 .no): ')
-}while(opcion != '2')
+  do {
+    opcion = prompt("Desea ingresar otra vivienda? (1. si - 2 .no): ");
+  } while (opcion != "1" && opcion != "2");
+} while (opcion != "2");
