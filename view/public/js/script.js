56dd3bd08a3bef76ce202a8ea400f07375b49c69
nataliagigo <nataliagigo@hotmail.es>
2024-10-25 11:32:46 -0500

    se limito el numero de ingresos a 3 y se creo una pagina de error para direccionar

--- view/public/js/script.js
+++ view/public/js/script.js
@@ -1,11 +1,32 @@
 import { buscarUsuario } from "../../../controller/controllerUsuarios.js";
 
 document.getElementById("btnIniciar").addEventListener("click", iniciarSesion);
-
+let contador = 0
 function iniciarSesion(){
-  if(buscarUsuario()){
-    window.location.href = '/view/pages/home.html' //redireccionar
-  } else {
-    console.log('Error de credenciales')
-  }
-}
+        // if(buscarUsuario()){  
+        //     if(contador <= 3){
+        //         window.location.href = '/view/pages/home.html' //redireccionar
+        //     }else{
+        //         return
+        //     }  
+        // } else {
+        //     contador ++
+        //     console.log('Error de credenciales')
+        //     console.log('numero de intentos: ' + contador)
+        // }
+
+
+        if(!buscarUsuario()){  
+            contador ++
+            console.log('Error de credenciales')
+            console.log('numero de intentos: ' + contador)
+            if(contador >= 3){
+                window.location.href = '/view/pages/error.html'
+                throw new Error('Numero máximo de intentos permitidos')
+            }
+        } else {
+            if(contador <= 3){
+                window.location.href = '/view/pages/home.html' //redireccionar
+            } 
+        }
+    }
